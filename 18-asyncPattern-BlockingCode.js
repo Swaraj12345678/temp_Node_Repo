166143fa3365eb3ce8c77003e938260d09bbcdc7
Swaraj12345678 <swaraj.nalawade04@gmail.com>
2024-06-21 15:54:30 +0530

    third commit

---
+++ 18-asyncPattern-BlockingCode.js
@@ -0,0 +1,29 @@
+// async patterns -> blocking code!
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.end("Welcome to the homepage");
+    }
+    else if (req.url === '/about') {
+        // BLOCKING CODE
+        for (let i = 0; i < 100; i++) {
+            for (let j = 0; j < 10; j++) {
+                console.log(`Blocking Code!`);
+            }
+        }
+        res.end("Welcome to the about page!");
+    }
+    else {
+        res.end(`
+            <h1>Oops!!</h1>
+            <p> We can't seem to find the page you are looking for </p>
+            <a href="/">Back home</a>            
+        `)
+    }
+});
+
+server.listen(5000, () => {
+    console.log("Server is listening on Port 5000....");
+});
